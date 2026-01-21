d7614e42396e4963d0fd382507a8a0a11b5a57d5
Tuvshu0-coder <s24c080b@nmct.edu.mn>
2026-01-22 01:25:25 +0800

    Checkpoint

--- КУ-2 Э.Түвшинзаяа/USACO Silver/Lemonade_Line.cpp
+++ КУ-2 Э.Түвшинзаяа/USACO Silver/Lemonade_Line.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 #define int long long
 
+bool isGreater(int a, int b){
+    return a > b;
+}
+
 void solve(){
-    
+    freopen("lemonade.in", "r", stdin);
+    freopen("lemonade.out", "w", stdout);
     int N;
     cin >> N;
     vector<int> arr(N);
     for(int &t : arr) cin >> t;
+    int minCow = 0;
+    sort(arr.begin(), arr.end() , isGreater);
+    for(int i=0; i<N; i++){
+        if(i <= arr[i]) minCow ++;
+        else{
+            break;
+        }
+    }
+    cout << minCow;
     
     
     
